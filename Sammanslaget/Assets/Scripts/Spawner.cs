295efd504d5722f12ad26d3aac038903dd5f321e
Carl Christensen <carl.christensen98@gmail.com>
2022-08-23 11:38:27 +0200

    Made different pickups

--- Sammanslaget/Assets/Scripts/Spawner.cs
+++ Sammanslaget/Assets/Scripts/Spawner.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject pickUp;
+    public GameObject cherry, bottle;
     private float startTime;
     private float height;
     private int randomHeight;
+    private int spawnIndex;
+    private List<GameObject> spawnList = new List<GameObject>();
 
     void Start()
     {
         startTime = Time.time;
+        spawnList.Add(cherry);
+        spawnList.Add(bottle);
+
         SetHeight();
+        SetSpawnObject();
     }
 
     void Update()
     {
         if(Time.time > startTime + 1.5f)
         {
-            Instantiate(pickUp, new Vector2(10, height), Quaternion.identity);
+            Instantiate(spawnList[spawnIndex], new Vector2(8.5f, height), Quaternion.identity);
             startTime = Time.time;
             SetHeight();
+            SetSpawnObject();
         }
+    }
+
+    private void SetSpawnObject()
+    {
+        spawnIndex = Random.Range(0, spawnList.Count);
     }
 
     void SetHeight()
